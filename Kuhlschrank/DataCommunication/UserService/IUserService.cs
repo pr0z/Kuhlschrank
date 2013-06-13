9543a11a57fc398ca80c08925f05b01d894bdbf4
levalidator <levalidator@gmail.com>
2013-06-13 19:28:52 +0200

    Ajout DataCommunication

---
+++ Kuhlschrank/DataCommunication/UserService/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using DataContracts;
+
+
+namespace DataCommunication.UserService
+{
+    [ServiceContract]
+    public interface IUserService
+    {
+        [OperationContract]
+        User GetUser(int id, string nom);
+    }
+}
---
+++ Kuhlschrank/DataCommunication/UserService/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataContracts;
+
+namespace DataCommunication.UserService
+{
+    public class UserService : IUserService
+    {
+        public User GetUser(int id, string nom)
+        {
+            return new User { Id = id, Nom = nom };
+        }
+    }
+}

0a75b50aa7f39d2a8aba84016124153c8ddfb46b
levalidator <levalidator@gmail.com>
2013-06-13 19:28:06 +0200

    rename + serialization

---
+++ Kuhlschrank/DataContracts/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DataContracts
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nom { get; set; }
+        [DataMember]
+        public string Prenom { get; set; }
+        [DataMember]
+        public string Mail { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+    }
+}

55938ad4f39c5cb0b195a9e2f51e3a1e0d7d654d
levalidator <levalidator@gmail.com>
2013-06-22 21:37:19 +0200

    ajout RepoList

---
+++ Kuhlschrank/DataAccess/UserListRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DataAccess.Repository;
+using DataContracts;
+
+namespace DataAccess
+{
+    public class UserListRepository : IUserRepository
+    {
+        private static List<User> _source;
+
+        public UserListRepository()
+        {
+            if (_source == null)
+            {
+                _source = new List<User>();
+                Insert(new User
+                {
+                    ID = 0,
+                    Mail = "test@test.fr",
+                    Nom = "test",
+                    Prenom = "test",
+                    Password = "test"
+                });
+            }
+        }
+
+        #region IUserRepository Membres
+
+        public User GetUserFromIdAndPassword(string identifier, string password)
+        {
+            return _source.Where(o => o.Mail == identifier && o.Password == password).FirstOrDefault();
+        }
+
+        #endregion
+
+        #region IRepository<User> Membres
+
+        public User GetById(int id)
+        {
+            return _source.Where(o => o.ID == id).FirstOrDefault();
+        }
+
+        public List<User> GetAll()
+        {
+            return _source;
+        }
+
+        public void Insert(User entity)
+        {
+            _source.Add(entity);
+        }
+
+        public void Update(User entity)
+        {
+            User item = _source.FirstOrDefault(o => o.ID == entity.ID);
+            if (item != null)
+            {
+                item.Mail = entity.Mail;
+                item.Nom = entity.Nom;
+                item.Password = entity.Password;
+                item.Prenom = entity.Prenom;
+            }
+        }
+
+        public void Delete(User entity)
+        {
+            User item = _source.FirstOrDefault(o => o.ID == entity.ID);
+            if (item != null)
+            {
+                _source.Remove(item);
+            }
+        }
+
+        #endregion
+    }
+}

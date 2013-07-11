98e8bd04c639d3585cd65b8ee99ed55432d52582
Benjamin GRAUX <benjamin.graux@gmail.com>
2013-07-12 00:52:39 +0200

    Premiers tests unitaire

--- Kuhlschrank/UnitTests/KuhlschrankTest.cs
+++ Kuhlschrank/UnitTests/KuhlschrankTest.cs
@@ -3,6 +3,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using DataAccess.ProductRepositoriesImplementation;
 using System.Collections.Generic;
 using DataContracts;
+using DataAccess.CategoryRepositoriesImplementation;
+using Common.Repositories.ProductRepository;
+using Common.Repositories.CategoryRepository;
 
 namespace UnitTests
 {
@@ -18,15 +21,36 @@ namespace UnitTests
 
             Assert.IsTrue(result != null && result.Count > 0);
         }
-
+        
         [TestMethod]
         public void ShouldReturnProduct()
         {
             Product result = null;
-            ProductListRepository repo = new ProductListRepository();
+            IProductRepository repo = new ProductListRepository();
             result = repo.GetById(1);
 
             Assert.IsTrue(result != null && result is Product);
         }
+     
+        [TestMethod]
+        public void ShouldReturnProductWithEan()
+        {
+            Product result = null;
+            ProductListRepository repo = new ProductListRepository();
+            result = repo.GetByEan13("");
+
+            Assert.IsTrue(result == null);
+        }
+
+        [TestMethod]
+        public void ShouldReturnCategory()
+        {
+            Category result = null;
+            ICategoryRepository repo = new CategoryListRepository();
+            result = repo.GetById(0);
+
+            Assert.IsTrue(result != null && result is Category);
+        }
+
     }
 }
